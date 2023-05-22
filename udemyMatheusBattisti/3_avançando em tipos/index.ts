192a7dd14a59e246f03d0f03397c4144c6e934f3
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-05-22 10:12:51 -0300

    err

--- udemyMatheusBattisti/3_avançando em tipos/index.ts
+++ udemyMatheusBattisti/3_avançando em tipos/index.ts
@@ -1,42 +1,9 @@
-// 1 - numbers
-let x: number = 10
+// 1 arrays
+let numbers:number[] = [1,2,3]
 
-console.log(x)
+numbers.push(5)
+console.log(numbers[2])
+// numbers = 'teste'
 
-x = 16
-console.log(typeof x)
-
-const y: number = 17.11111111
-console.log(typeof y)
-console.log(y.toPrecision(3))
-
-//  2  - string
-const firstName: string = 'Nelson'
-console.log(firstName.toUpperCase())
-
-const lastName: string = 'Carvalho'
-console.log(lastName.toUpperCase())
-
-fullName = firstName + ' ' + lastName
-console.log(fullName)
-
-console.log(typeof fullName)
-
-// 3 -  boolean
-let a: boolean = false
-console.log(a)
-console.log(typeof a)
-
-a = true
-console.log(a)
-
-//  4 - inference e annotation
-
-const ann: string = 'String através de annotation :'
-let inf = 'String através de Inference' 
-
-// inf = 10
-
-let nome: string = 'Nelson'
-console.log(nome)
-
+const nomes: string[] = ['Nelson','Carvalho']
+// nomes.push(4)
