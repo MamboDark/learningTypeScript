c9b7c472c53711948441dc5912c94c677052a9a1
Nelson Carvalho <nelson-carvalho@live.com>
2023-05-27 21:17:34 -0300

    revendo.

--- udemyMatheusBattisti/1_Intro/index.js
+++ udemyMatheusBattisti/1_Intro/index.js
@@ -1,9 +1,12 @@
 var firstName = 'Nelson';
+var firstNameCaps = 'NELSON';
 var anotherName = 1;
 var x = true;
 function greeting(name) {
     console.log('Hello ' + name);
+    console.log("HELLO WORLD ".concat(name, " :D"));
 }
 greeting(firstName);
-// greeting(anotherName)
-// greeting(x)
+greeting(firstNameCaps);
+//greeting(anotherName)
+//greeting(x)
--- udemyMatheusBattisti/1_Intro/index.ts
+++ udemyMatheusBattisti/1_Intro/index.ts
@@ -1,10 +1,13 @@
 const firstName = 'Nelson'
+const firstNameCaps = 'NELSON'
 const anotherName = 1
 const x = true
 
 function greeting(name: string){
     console.log('Hello ' + name)
+    console.log(`HELLO WORLD ${name} :D`)
 }
 greeting(firstName)
-// greeting(anotherName)
-// greeting(x)
+greeting(firstNameCaps)
+//greeting(anotherName)
+//greeting(x)
